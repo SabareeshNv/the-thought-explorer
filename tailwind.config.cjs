272c9e281400e50788069ec15ac54fc1317cf64a
Sabareesh N V <sabareeshnv@gmail.com>
2023-03-06 23:34:51 +0530

    changed looks

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,21 +2,22 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Inter", ...defaultTheme.fontFamily.sans],
-				heading: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
-				bitter: ['Bitter', 'serif'],
-			},
-			colors: {
-				'accent': "#CCE6D0",
-				'brand-white': "#FCFAFF",
-			},
-		},
-	},
-	plugins: [
-		require("@tailwindcss/typography")
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        heading: ["Bitter", ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        accent: "#CCE6D0",
+        "brand-white": "#FCFAFF",
+        "brand-black": "#2B2B2B",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};
